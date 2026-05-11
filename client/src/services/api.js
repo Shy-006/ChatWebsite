c374085560410f8e99e52678b5105f38e8c0119b
Shyam <shyamnathab2006@gmail.com>
2026-05-11 14:45:33 +0530

    Fix token refresh interceptor for 401s

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -12,7 +12,7 @@ api.interceptors.response.use((response) => {
   return response;
 }, async (error) => {
   const originalRequest = error.config;
-  if (error.response && error.response.status === 403 && !originalRequest._retry) {
+  if (error.response && (error.response.status === 403 || error.response.status === 401) && !originalRequest._retry && originalRequest.url !== `${API_URL}/auth/refresh-token`) {
     originalRequest._retry = true;
     try {
       const res = await axios.post(`${API_URL}/auth/refresh-token`, {}, { withCredentials: true });
